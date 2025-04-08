1b098a571e3d4a2aa8d077d29a77b6ead3be81bd
非凡少年 <hutyxxx@gmail.com>
2025-04-08 21:35:35 +0800

    Update list.tsx

--- src/app/(full-layout)/keep/components/list.tsx
+++ src/app/(full-layout)/keep/components/list.tsx
@@ -92,7 +92,7 @@ export function KeepList() {
       loading={isFetchingNextPage}
       hasMore={hasNextPage}
     >
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         <AnimatePresence mode="popLayout">
           {allKeeps.map(keep => (
             <KeepCard key={keep.id} keep={keep} />
